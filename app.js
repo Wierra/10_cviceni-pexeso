e0b3a6f1832f1ce8b50774f1e84da1b54a22b43a
Věra Juračáková <juracakova.vera@gmail.com>
2024-12-08 20:36:28 +0100

    Úprava 08.12.2024

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
-console.log('Funguju!')
+const karticky = document.querySelectorAll('.karticka');
 
-const karticky = document.querySelectorAll('.karticka')
+let otoceneKarty = [];
 
-karticky[0].classList.remove('otocena')
+karticky.forEach((karticka, index) => {
+  karticka.addEventListener('click', (event) => {
+    karticka.classList.remove('otocena');
+    otoceneKarty.push(index);
+
+    if (otoceneKarty.length === 2) {
+      const karta1 = karticky[otoceneKarty[0]];
+      const karta2 = karticky[otoceneKarty[0]];
+      const obrazekKarta1 = karticky[otoceneKarty[0]].querySelector('img');
+      const obrazekKarta2 = karticky[otoceneKarty[1]].querySelector('img');
+
+      if (obrazekKarta1.src != obrazekKarta2.src) {
+        otoceneKarty.forEach((indexKarticky) => {
+          setTimeout(() => {
+            karticky[indexKarticky].classList.add('otocena');
+          }, 1000);
+        });
+        otoceneKarty = [];
+      } else {
+        karta1.disabled = true;
+        karta2.disabled = true;
+        otoceneKarty = [];
+      }
+    }
+  });
+});
